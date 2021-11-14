184636dabf83454fca3013f80bbe74b244fe71af
Alexei Ptitchkin <ptitchkin@gmail.com>
2021-11-14 18:43:28 -0500

    update test case to use printRow(row, keys)

--- bookmarkReaderutils.py
+++ bookmarkReaderutils.py
@@ -1,3 +1,21 @@
+# Print row instead of accessing row directly with index
+# #rows keys: id, url, title, visit_count, typed_count, last_visit_time, hidden
+# keys = ['id', 'url', 'title', 'visit_count', 'typed_count', 'last_visited', 'hidden']
+# for row in cursor:
+#     rowStr = printRow(row, keys)
+#     print(rowStr)
+#
+# old code
+# for row in cursor:
+#     print("ID:", row[0])
+#     print("URL:", row[1])
+#     print("Title:", row[2])
+#     print("Visit Count:", row[3])
+#     print("Typed Count:", row[4])
+#     print("Last Visit Time:", row[5])
+#     print("Hidden:", row[6])
+#     print("\n")
+
 def printRow(row, keys):
     for key in keys:
         buffer = "{"
@@ -8,6 +26,7 @@ def printRow(row, keys):
         buffer = buffer[:-1]
         buffer += "}"
         return buffer
+
 
 def readFolder(folder, toArray, level=0):
     # construct indent on level 
--- testSqlLightHistoryDate.py
+++ testSqlLightHistoryDate.py
@@ -1,5 +1,6 @@
 import sqlite3
 from sqlite3 import Error
+from bookmarkReaderutils import *
 
 # connect  to SqlLight db and write urls to table
 try:
@@ -10,28 +11,13 @@ try:
 
     #cursor = conn.execute("SELECT urls.id, urls.url, urls.title, urls.visit_count, urls.typed_count, datetime(last_visit_time,'unixepoch'), urls.hidden FROM urls")
     cursor = conn.execute("SELECT id, url, title, visit_count, typed_count, datetime(last_visit_time/1000000-11644473600, \"unixepoch\") as last_visited, hidden FROM urls ORDER BY visit_count DESC")
-    # for row in cursor:
-    #     print("ID:", row[0])
-    #     print("URL:", row[1])
-    #     print("Title:", row[2])
-    #     print("Visit Count:", row[3])
-    #     print("Typed Count:", row[4])
-    #     print("Last Visit Time:", row[5])
-    #     print("Hidden:", row[6])
-    #     print("\n")
 
     # rows keys: id, url, title, visit_count, typed_count, last_visit_time, hidden
-    keys = ['id', 'url', 'title', 'visit_count', 'typed_count', 'last_visit_time', 'hidden']
+    keys = ['id', 'url', 'title', 'visit_count', 'typed_count', 'last_visited', 'hidden']
     max = 10
     for row in cursor:
-        buffer = "{"
-        # loop through keys and add row[key index] to buffer
-        for key in keys:
-            buffer += '"' + key + '":"' + str(row[keys.index(key)]) + '",'
-        # remove last comma
-        buffer = buffer[:-1]
-        buffer += "}"
-        print(buffer)
+        rowStr = printRow(row, keys)
+        print(rowStr)
         max -= 1
         if max == 0:
             break
